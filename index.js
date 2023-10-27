66776e1a320a5e95eebe7937eb42a39401393505
DoctorTan <87746911+DoctorTan@users.noreply.github.com>
2023-10-27 23:59:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /**
  * 2023年10月24日 23:24:18
  * 支持解析痞客邦相册
@@ -9,7 +8,6 @@
 // let pixnetAlbumNameUrl = 'https://abbey1117.pixnet.net/album/list'//多相册
 // let pixnetAlbumNameUrl = 'https://ohiyuo.pixnet.net/album/list'
 // let pixnetAlbumNameUrl = 'https://rm8532.pixnet.net/album/list'
-let pixnetAlbumNameUrl = ''
 
 let picNum = 0
 const startTime = new Date();
@@ -26,7 +24,7 @@ let i = 0 //计算照片数量
 // let baseUrl = url.parse(pixnetAlbumNameUrl).protocol + '//' + url.parse(pixnetAlbumNameUrl).host
 let baseUrl = ''
 //下一个后半部分url
-let nextPageLink = ''
+// let pixnetAlbumNameUrl = ''
 
 //获取图片和下一个网页函数
 console.log('解析中');
@@ -37,99 +35,83 @@ let response
 let html
 let $
 let photoLinks
-const readline = require('readline');
+(async () => {
+  let config = await readAlbumUrlFromLocalConfig()
+  //获取从本地读取的文件
+  pixnetAlbumNameUrl = config.pixnetAlbumNameUrl
+  // console.log(config.pixnetAlbumNameUrl);
+  baseUrl = url.parse(pixnetAlbumNameUrl).protocol + '//' + url.parse(pixnetAlbumNameUrl).host
+  console.time("解析所有照片时间"); // 开始计时
+  response = await axios.get(pixnetAlbumNameUrl);
+  html = response.data;
+  $ = cheerio.load(html);//取html页面 
+  if (pixnetAlbumNameUrl.includes("album/list")) {
+    console.log('解析到是多相册');
+    photoLinks = $('.album-grid-list .photolink');
+    photoLinks.each(async (index, element) => {
+      // 先解析相册第一张
+      AlbumList.push(element.attribs.href)
+      // console.log('解析到相册', element.attribs.href);  
+    });
+  } else {
+    console.log('解析到是单相册');
+    AlbumList.push(pixnetAlbumNameUrl)
+  }
+  // console.log(AlbumList);
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-function inputUUrl() {
-  rl.question('请输入相册链接或者相册列表链接：', (answer) => {
-    if (answer.includes('pixnet.net/album/set') || answer.includes('pixnet.net/album/list')) {
-      pixnetAlbumNameUrl = answer
-      rl.close();
-      (async () => {
-        baseUrl = url.parse(pixnetAlbumNameUrl).protocol + '//' + url.parse(pixnetAlbumNameUrl).host
-        console.time("解析所有照片时间"); // 开始计时
-        response = await axios.get(pixnetAlbumNameUrl);
-        html = response.data;
-        $ = cheerio.load(html);//取html页面 
-        if (pixnetAlbumNameUrl.includes("album/list")) {
-          console.log('解析到是多相册');
-          photoLinks = $('.album-grid-list .photolink');
-          photoLinks.each(async (index, element) => {
-            // 先解析相册第一张
-            AlbumList.push(element.attribs.href)
-            // console.log('解析到相册', element.attribs.href);  
-          });
-        } else {
-          console.log('解析到是单相册');
-          AlbumList.push(pixnetAlbumNameUrl)
-        }
-        // console.log(AlbumList);
+  //   let chunks = []
+  //   let chunksPromise = []
+  //   let limit = 3//
+  //   // 已经被分割为一个小小的数组了
+  //   for (let i = 0; i < AlbumList.length; i += limit) {
+  //     chunks.push(AlbumList.slice(i, i + limit))
+  //   }
+  // // console.log(chunks);
+  //   //重复并发相册
+  //   for (let i = 0; i < chunks.length; i++) {
+  //     chunksPromise.push(new Promise(async (resolve, reject) => {
+  //       //这里存的是每个chunk中的每一个url,有1000个就是存入1000个promise
+  //       let chunkPromise = []
+  //       console.log(`正在解析第${i}个chuck`);
+  //       for (let j = 0; j < chunks[i].length; j++) {
+  //         chunkPromise.push(getAllPicUrl(chunks[i][j]))
+  //         // console.log(chunks[i][j]);
+  //       }
+  //       // console.log(chunkPromise);
+  //       await Promise.allSettled(chunkPromise).then(() => {
+  //         console.log(chunkPromise, '解析完了第', i + 1, '个chunk');
+  //         resolve('解析完一个promise')
+  //       })
+  //     }))
+  //   }
+  //   // console.log('妈的',chunksPromise,chunks.length);
 
-        //   let chunks = []
-        //   let chunksPromise = []
-        //   let limit = 3//
-        //   // 已经被分割为一个小小的数组了
-        //   for (let i = 0; i < AlbumList.length; i += limit) {
-        //     chunks.push(AlbumList.slice(i, i + limit))
-        //   }
-        // // console.log(chunks);
-        //   //重复并发相册
-        //   for (let i = 0; i < chunks.length; i++) {
-        //     chunksPromise.push(new Promise(async (resolve, reject) => {
-        //       //这里存的是每个chunk中的每一个url,有1000个就是存入1000个promise
-        //       let chunkPromise = []
-        //       console.log(`正在解析第${i}个chuck`);
-        //       for (let j = 0; j < chunks[i].length; j++) {
-        //         chunkPromise.push(getAllPicUrl(chunks[i][j]))
-        //         // console.log(chunks[i][j]);
-        //       }
-        //       // console.log(chunkPromise);
-        //       await Promise.allSettled(chunkPromise).then(() => {
-        //         console.log(chunkPromise, '解析完了第', i + 1, '个chunk');
-        //         resolve('解析完一个promise')
-        //       })
-        //     }))
-        //   }
-        //   // console.log('妈的',chunksPromise,chunks.length);
+  //    Promise.all(chunksPromise).then(res=>{
+  //     console.log('所有都解析完-----------------------------------------------的点点滴滴----------------------');
+  //   })
+  //   // console.log('所有都解析完');
+  //   return
+  Promise.all(AlbumList.map((promise) => getAllPicUrl(promise))).then(res => {
+    let totalListCount = picNum
+    const endTimeCollect = new Date();
+    const timeTakenCollect = endTimeCollect - startTime;
+    //上面是解析完, 下面是全部写入完
+    Promise.all(allPromise).then(res => {
+      const endTimeWrite = new Date();
+      const timeTakenWrite = endTimeWrite - startTime;
+      console.log('总共写入:', i, '张');
+      console.log("解析相册照片总数:", totalListCount, '张', '\n解析相册照片耗时:', timeTakenCollect, '毫秒', '\n写入操作耗时:', timeTakenWrite, '毫秒');
+      // console.log(`解析所有照片耗时：${timeTakenCollect} 毫秒,操作完需要${timeTakenWrite} 毫秒,`);
+    })
 
-        //    Promise.all(chunksPromise).then(res=>{
-        //     console.log('所有都解析完-----------------------------------------------的点点滴滴----------------------');
-        //   })
-        //   // console.log('所有都解析完');
-        //   return
-        Promise.all(AlbumList.map((promise) => getAllPicUrl(promise))).then(res => {
-          let totalListCount = picNum
-          const endTimeCollect = new Date();
-          const timeTakenCollect = endTimeCollect - startTime;
-          //上面是解析完, 下面是全部写入完
-          Promise.all(allPromise).then(res => {
-            const endTimeWrite = new Date();
-            const timeTakenWrite = endTimeWrite - startTime;
-            console.log('总共写入:', i, '张');
-            console.log("解析相册照片总数:", totalListCount, '张', '\n解析相册照片耗时:', timeTakenCollect, '毫秒', '\n写入操作耗时:', timeTakenWrite, '毫秒');
-            // console.log(`解析所有照片耗时：${timeTakenCollect} 毫秒,操作完需要${timeTakenWrite} 毫秒,`);
-          })
-
-        })
-      })()
-    } else {
-      console.log('URL 包含 "pixnet.net/album/set" 或 "pixnet.net/album/list"');
-      inputUUrl()
-    }
-
-
-  });
-
-}
-
+  })
+})()
+let allPic = [];
 
 let allPromise = []
 async function getAllPicUrl(url) {
 
-
+  // let baseUrl = url.parse(pixnetAlbumNameUrl).protocol + '//' + url.parse(pixnetAlbumNameUrl).host
   let nextPageLink = url;
   let response = await axios.get(url);
   let html = response.data;
@@ -137,7 +119,7 @@ async function getAllPicUrl(url) {
   let albumName = $('meta[name="description"]').attr('content');
   albumName = filterPathName(albumName)
   // console.log('一个相册对象一次');
-  const dir = path.join(process.cwd(), albumName);
+  const dir = path.join(__dirname, albumName);
 
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
@@ -228,7 +210,26 @@ async function saveImagesToLocal(imageUrls, albumName) {
     console.error('保存图片时发生错误：', error);
   }
 }
-inputUUrl()
 
 
+function readAlbumUrlFromLocalConfig() {
+  const filePath = path.join(__dirname, 'config.json');
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('读取文件出错:', err);
+        return;
+      }
 
+      try {
+        const config = JSON.parse(data);
+        // console.log(config);
+        resolve(config)
+        // pixnetAlbumNameUrl = config.pixnetAlbumNameUrl
+      } catch (error) {
+        console.error('解析 JSON 出错:', error);
+      }
+    });
+  })
+  // return pixnetAlbumNameUrl
+}
